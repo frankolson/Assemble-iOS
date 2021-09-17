d3de999be5bbd2913fb00e65e9a4aedf044a7379
Will Olson <will.olson11@gmail.com>
2021-09-17 11:40:20 -0700

    Create splash screen

---
+++ Assemble/Views/Colors.swift
@@ -0,0 +1,13 @@
+//
+//  Colors.swift
+//  Assemble
+//
+//  Created by Will Olson on 9/17/21.
+//
+
+import UIKit
+
+extension UIColor {
+    static let primary = UIColor(named: "Primary")!
+    static let primaryLight = UIColor(named: "PrimaryLight")!
+}
